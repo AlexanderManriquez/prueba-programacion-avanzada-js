16ac348f9ec0940f3894221e9b42fa513fa6874f
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-10-05 09:29:49 -0300

    adding async/await for img load

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,53 +4,32 @@ import { Lobo } from "./lobo.js";
 import { Oso } from "./oso.js";
 import { Serpiente } from "./serpiente.js";
 
-document.getElementById("btnRegistrar").addEventListener("click", () => {
+const animales = {
+  Aguila: "../assets/imgs/Aguila.png",
+  Leon: "../assets/imgs/Leon.png",
+  Lobo: "../assets/imgs/Lobo.jpg",
+  Oso: "../assets/imgs/Oso.jpg",
+  Serpiente: "../assets/imgs/Serpiente.jpg",
+};
+
+document.getElementById("btnRegistrar").addEventListener("click", async () => {
   const nombreAnimal = document.getElementById("animal").value;
   const edadAnimal = document.getElementById("edad").value;
   const comentarios = document.getElementById("comentarios").value;
-  let animal;
+  
+  const imgSrc = await loadImage(animales[nombreAnimal]);
 
-  // Crear instancia según el animal seleccionado
+  let animal;
   if (nombreAnimal === "Aguila") {
-    animal = new Aguila(
-      nombreAnimal,
-      edadAnimal,
-      "../assets/imgs/Aguila.png",
-      comentarios,
-      "../assets/sounds/Chillido.mp3"
-    );
+    animal = new Aguila(nombreAnimal, edadAnimal, imgSrc, comentarios, "../assets/sounds/Chillido.mp3");
   } else if (nombreAnimal === "Leon") {
-    animal = new Leon(
-      nombreAnimal,
-      edadAnimal,
-      "../assets/imgs/Leon.png",
-      comentarios,
-      "../assets/sounds/Rugido.mp3"
-    );
+    animal = new Leon(nombreAnimal, edadAnimal, imgSrc, comentarios, "../assets/sounds/Rugido.mp3");
   } else if (nombreAnimal === "Lobo") {
-    animal = new Lobo(
-      nombreAnimal,
-      edadAnimal,
-      "../assets/imgs/Lobo.jpg",
-      comentarios,
-      "../assets/sounds/Aullido.mp3"
-    );
+    animal = new Lobo(nombreAnimal, edadAnimal, imgSrc, comentarios, "../assets/sounds/Aullido.mp3");
   } else if (nombreAnimal === "Oso") {
-    animal = new Oso(
-      nombreAnimal,
-      edadAnimal,
-      "../assets/imgs/Oso.jpg",
-      comentarios,
-      "../assets/sounds/Grunido.mp3"
-    );
+    animal = new Oso(nombreAnimal, edadAnimal, imgSrc, comentarios, "../assets/sounds/Grunido.mp3");
   } else if (nombreAnimal === "Serpiente") {
-    animal = new Serpiente(
-      nombreAnimal,
-      edadAnimal,
-      "../assets/imgs/Serpiente.jpg",
-      comentarios,
-      "../assets/sounds/Siseo.mp3"
-    );
+    animal = new Serpiente(nombreAnimal, edadAnimal, imgSrc, comentarios, "../assets/sounds/Siseo.mp3");
   }
 
   document.getElementById("preview").innerHTML = `
@@ -60,13 +39,22 @@ document.getElementById("btnRegistrar").addEventListener("click", () => {
   // Agregar el animal a la tabla de la izquierda (div #Animales)
   document.getElementById("Animales").innerHTML += `
     <div class="card m-2 p-2 bg-light text-dark" style="width: 10rem;">
-      <img src="${animal.img}" class="card-img-top animal-img" alt="${animal.nombre}" onclick="showDetails('${animal.nombre}', '${animal.edad}', '${animal._comentarios}', '${animal.img}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <img src="${animal.img}" class="card-img-top  animal-img" alt="${animal.nombre}" onclick="showDetails('${animal.nombre}', '${animal.edad}', '${animal._comentarios}', '${animal.img}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
       <div class="card-body">
         <button class="btn btn-dark" onclick="playSound('${animal.sonido}')">Reproducir Sonido</button>
       </div>
     </div>
   `;
 });
+
+async function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve(img.src);
+    img.onerror = reject;
+  });
+}
 
 window.playSound = function (sonido) {
   const player = document.getElementById("player");
@@ -75,14 +63,12 @@ window.playSound = function (sonido) {
 };
 
 window.showDetails = function (nombre, edad, comentarios, imgSrc) {
-    const modalBody = document.querySelector(".modal-body");
-    modalBody.innerHTML = `
-      <div class="d-flex flex-column align-items-center text-center">
-        <img src="${imgSrc}" class="img-fluid mb-3" alt="${nombre}" style="max-width: 100%;">
-        <p><strong>Nombre:</strong> ${nombre}</p>
-        <p><strong>Edad:</strong> ${edad}</p>
-        <p><strong>Comentarios:</strong> ${comentarios}</p>
-      </div>
-    `;
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = `
+    <img src="${imgSrc}" class="img-fluid mb-3" alt="${nombre}">
+    <p><strong>Nombre:</strong> ${nombre}</p>
+    <p><strong>Edad:</strong> ${edad}</p>
+    <p><strong>Comentarios:</strong> ${comentarios}</p>
+  `;
 };
 
